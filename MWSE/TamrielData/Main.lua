7dee894c2cc143c45f3c72fe3a1d51c7affb835a
Ell NZ <56051875+Ellnz@users.noreply.github.com>
2021-03-06 22:55:47 +1300

    Create Main.lua

---
+++ MWSE/TamrielData/Main.lua
@@ -0,0 +1,9 @@
+local ashfall = include("mer.ashfall.interop")
+if ashfall then
+
+#    ashfall.registerActivators{
+#        mymod_water_01 = "water",
+#        mymod_ex_log_01 = "wood"
+#    }
+
+end
